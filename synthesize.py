46c317c0746cf649dad6f8c63891e944816f9e63
dahyunnss <hyeons.opy@gmail.com>
2024-09-22 03:25:52 +0900

    test_gpu

--- synthesize.py
+++ synthesize.py
@@ -4,9 +4,11 @@ from string import punctuation
 import torch
 import yaml
 import numpy as np
+
 import os
 import json
-
+import datetime
+import time
 import librosa
 import pyworld as pw
 import audio as Audio
@@ -20,8 +22,26 @@ from utils.tools import to_device, synth_samples
 from dataset import BatchInferenceDataset
 from text import text_to_sequence
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+from pymcd.mcd import Calculate_MCD
+from pesq import pesq
+from scipy.io import wavfile
 
+mcd_toolbox = Calculate_MCD(MCD_mode="plain")
+
+def calculate_rtf(audio_length, processing_time):
+    return processing_time / audio_length
+
+def calculate_pesq(ref_wav, synth_wav, sr=16000):
+    return pesq(sr, ref_wav, synth_wav, 'wb')
+
+def create_log_file():
+    timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    log_filename = f"mcd_log_{timestamp}.txt"
+    return log_filename
+
+def log_data(log_filename, data):
+    with open(log_filename, "a") as file:
+        file.write(data + "\n")
 
 def read_lexicon(lex_path):
     lexicon = {}
@@ -36,7 +56,6 @@ def read_lexicon(lex_path):
 
 
 def get_audio(preprocess_config, wav_path):
-
     hop_length = preprocess_config["preprocessing"]["stft"]["hop_length"]
     sampling_rate = preprocess_config["preprocessing"]["audio"]["sampling_rate"]
     STFT = Audio.stft.TacotronSTFT(
@@ -105,45 +124,58 @@ def preprocess_english(text, preprocess_config):
             phones, preprocess_config["preprocessing"]["text"]["text_cleaners"]
         )
     )
-
     return np.array(sequence)
 
-
-def preprocess_mandarin(text, preprocess_config):
-    lexicon = read_lexicon(preprocess_config["path"]["lexicon_path"])
-
-    phones = []
-    pinyins = [
-        p[0]
-        for p in pinyin(
-            text, style=Style.TONE3, strict=False, neutral_tone_with_five=True
-        )
-    ]
-    for p in pinyins:
-        if p in lexicon:
-            phones += lexicon[p]
-        else:
-            phones.append("sp")
-
-    phones = "{" + " ".join(phones) + "}"
-    print("Raw Text Sequence: {}".format(text))
-    print("Phoneme Sequence: {}".format(phones))
-    sequence = np.array(
-        text_to_sequence(
-            phones, preprocess_config["preprocessing"]["text"]["text_cleaners"]
-        )
-    )
-
-    return np.array(sequence)
+def get_all_wav_files(directory):
+    wav_files = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.wav'):
+                wav_files.append(os.path.join(root, file))
+                
+    return sorted(wav_files)
 
 
-def synthesize(model, step, configs, vocoder, batchs, control_values):
+def calculate_pesq(ref_wav_path, synth_wav_path):
+    ref, ref_rate = librosa.load(ref_wav_path, sr=None)
+    synth, synth_rate = librosa.load(synth_wav_path, sr=None)
+    
+    # Ensure both signals have the same sampling rate
+    if ref_rate != synth_rate:
+        raise ValueError("Reference and synthesized files have different sampling rates")
+    
+    # Resample if needed
+    if ref_rate not in [8000, 16000]:
+        ref = librosa.resample(ref, orig_sr=ref_rate, target_sr=16000)
+        synth = librosa.resample(synth, orig_sr=synth_rate, target_sr=16000)
+        ref_rate = synth_rate = 16000
+    
+    ref = np.asarray(ref * 32768, dtype=np.int16)  # Convert to int16 format
+    synth = np.asarray(synth * 32768, dtype=np.int16)  # Convert to int16 format
+    
+    return pesq(ref_rate, ref, synth, 'wb')  # 'wb' is wideband, 'nb' is narrowband
+
+
+def synthesize(model, step, configs, vocoder, data_loader, control_values,log_filename,synthesized_wav_dir, reference_wav_dir, device):
+    print(f"Requested device: {device}")
+    print(f"CUDA available: {torch.cuda.is_available()}")
+    print(f"Current device: {torch.cuda.current_device() if torch.cuda.is_available() else 'CPU'}")
+    
     preprocess_config, model_config, train_config = configs
     pitch_control, energy_control, duration_control = control_values
+    mcd_scores = []
+    pesq_scores = [] 
+    inference_times = []
+    rtf_scores = []
 
-    for batch in batchs:
-        batch = to_device(batch, device)
+    print(f"Initial model device: {next(model.parameters()).device}")
+    print(f"Initial vocoder device: {next(vocoder.parameters()).device}")
+    
+    for idx, batch in enumerate(data_loader):
+        batch = to_device(batch, device) #gpu
         with torch.no_grad():
+            start = time.time() #gpu
+            
             # Forward
             output = model(
                 *(batch[2:-1]),
@@ -151,24 +183,77 @@ def synthesize(model, step, configs, vocoder, batchs, control_values):
                 e_control=energy_control,
                 d_control=duration_control
             )
+            end = time.time() #gpu
+            inf_time = end - start #gpu
+            inference_times.append(inf_time)
+            log_data(log_filename, f"Inference Time: {inf_time:} seconds")
+    
+            ref_mel = batch[-1][0][0]  
+            synth_mel = output[0].cpu().numpy()
+            synth_mel = synth_mel[0].transpose(1, 0)
+            
+            min_length = min(ref_mel.shape[1], synth_mel.shape[0])
+            ref_mel = ref_mel[:, :min_length]
+            synth_mel = synth_mel[:min_length, :]
+            
+            batch_synthesized_dir = os.path.join(synthesized_wav_dir, f"batch_{idx+1}")
+            os.makedirs(batch_synthesized_dir, exist_ok=True)
+            
+            print(f"Batch {idx+1}: Synthesizing samples...")
+            wav_start = time.time() #gpu
+            
             synth_samples(
                 batch,
                 output,
                 vocoder,
                 model_config,
                 preprocess_config,
-                train_config["path"]["result_path"],
+                batch_synthesized_dir,
             )
+            wav_end = time.time() #gpu
+            wav_gen_time = wav_end - wav_start#gpu
+
+            synthesized_files = sorted([f for f in os.listdir(batch_synthesized_dir) if f.endswith('.wav')])
+            reference_files = get_all_wav_files(reference_wav_dir)
+            
+            for synthesized_file, reference_file in zip(synthesized_files, reference_files):
+                synthesized_wav_path = os.path.join(batch_synthesized_dir, synthesized_file)
+                reference_wav_path = reference_file
+                
+                # Calculate audio length
+                audio, _ = librosa.load(synthesized_wav_path, sr=None)
+                audio_length = len(audio) / 22050  # Assuming 22050 Hz sample rate
+
+                # Calculate RTF
+                total_time = inf_time + wav_gen_time
+                rtf = calculate_rtf(audio_length, total_time)
+                rtf_scores.append(rtf)
+
+                # Calculate MCD and PESQ
+                mcd_value = mcd_toolbox.calculate_mcd(reference_wav_path, synthesized_wav_path)
+                mcd_scores.append(mcd_value)
+                
+                pesq_value = calculate_pesq(reference_wav_path, synthesized_wav_path)
+                pesq_scores.append(pesq_value)
+                
+                log_data(log_filename, f"{synthesized_file}: MCD Score = {mcd_value:}, PESQ Score = {pesq_value:}, RTF_GPU = {rtf:}")     
+
+    if mcd_scores or pesq_scores or inference_times:
+        average_mcd = sum(mcd_scores) / len(mcd_scores)
+        average_pesq = sum(pesq_scores) / len(pesq_scores)
+        avg_inf_time = sum(inference_times) / len(inference_times)
+        average_rtf = sum(rtf_scores) / len(rtf_scores)
+        log_data(log_filename, f"Average MCD Score: {average_mcd:}, Average PESQ Score: {average_pesq:}, Average RTF: {average_rtf:}")
+        log_data(log_filename, f"Average Inference Time:: {avg_inf_time:} seconds")
 
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser()
-    parser.add_argument("--restore_step", type=int, required=True)
+    parser.add_argument("--restore_step", type=int, required=True) #200000
     parser.add_argument(
         "--mode",
         type=str,
-        choices=["batch", "single"],
+        choices=["batch", "single"], #batch
         required=True,
         help="Synthesize a whole dataset or a single sentence",
     )
@@ -221,7 +306,27 @@ if __name__ == "__main__":
         default=1.0,
         help="control the speed of the whole utterance, larger value for slower speaking rate",
     )
+    parser.add_argument(
+        "--synthesized_wav_dir",
+        type=str,
+        required=True,
+        help="directory for synthesized wav files",
+    )
+    parser.add_argument(
+        "--reference_wav_dir",
+        type=str,
+        required=True,
+        help="directory for reference wav files",
+    )
+    parser.add_argument(
+        "--device",
+        type=str,
+        choices=["cpu", "cuda"],
+        default="cuda",
+        help="Device to run the model on (cpu or cuda)",
+    )
     args = parser.parse_args()
+    device = torch.device(args.device)
 
     # Check source texts
     if args.mode == "batch":
@@ -238,7 +343,7 @@ if __name__ == "__main__":
     configs = (preprocess_config, model_config, train_config)
 
     # Get model
-    model = get_model(args, configs, device, train=False)
+    model = get_model(args, configs, device, train=False) #error
 
     # Load vocoder
     vocoder = get_vocoder(model_config, device)
@@ -247,9 +352,10 @@ if __name__ == "__main__":
     if args.mode == "batch":
         # Get dataset
         dataset = BatchInferenceDataset(args.source, preprocess_config)
-        batchs = DataLoader(
+        data_loader = DataLoader(
             dataset,
-            batch_size=8,
+            batch_size=1,
+            shuffle=True,
             collate_fn=dataset.collate_fn,
         )
     if args.mode == "single":
@@ -264,5 +370,5 @@ if __name__ == "__main__":
             raw_texts, None, texts, text_lens, max(text_lens), mels, mel_lens, max(mel_lens), [ref_info])]
 
     control_values = args.pitch_control, args.energy_control, args.duration_control
-
-    synthesize(model, args.restore_step, configs, vocoder, batchs, control_values)
+    log_filename = create_log_file()
+    synthesize(model, args.restore_step, configs, vocoder, data_loader, control_values, log_filename,args.synthesized_wav_dir, args.reference_wav_dir,device)
